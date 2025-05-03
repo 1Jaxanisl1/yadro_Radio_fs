8a8310c67fbd7cd72b74bb45b7e97335d148bc4e
Andrey <jaxanisl1@gmail.com>
2025-05-04 02:20:59 +0300

    done all tasks for Radiofs

---
+++ Radio_frequency_system/README.md
@@ -0,0 +1,17 @@
+
+#Задача 2
+
+Файл: Hello, DevOps.sh
+
+#Задача 3
+
+Файл: syslog.sh
+
+#Задача 4
+
+Файл: bash_script.sh
+
+#Задача 5
+
+Файл с оптимизацией: Dockerfile.optimized
+Рабочий файл с оптимизацией: Dockerfile
--- Radio_frequency_system/bash_script.sh
+++ Radio_frequency_system/bash_script.sh
@@ -2,3 +2,12 @@
 
 # Считываем ввод
 
+read -p "Введите слово для поиска: " find_word
+read -p "Введите файл: " path_file
+
+if [[ -z "$find_word" && -z "$path_file" ]]; then
+    echo "Вы ничего не ввели!"
+    exit 1
+fi
+
+grep -i $find_word $path_file
